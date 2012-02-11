dd09dc3701ea0e643234a9bce2fb115803232a66
KOMURA Takaaki <emon@minidora.net>
2012-02-11 13:23:54 +0900

    テストパターンに何のためのテストかを追記

--- test.rb
+++ test.rb
@@ -3,19 +3,29 @@
 
 require './kana2hepburn'
 
-p 'あいうえお かきくけこ'.kana2hepburn
-p 'どらえもん'.kana2hepburn
-p 'きゃきゅきょ'.kana2hepburn
-p 'かんぽ'.kana2hepburn
-p 'ほんま'.kana2hepburn
-p 'へっぽこ'.kana2hepburn
-p 'はっさく'.kana2hepburn
-p 'うさぎ'.kana2hepburn
-p 'たろう'.kana2hepburn
-p 'おおさま'.kana2hepburn
-p 'きょうと'.kana2hepburn
-p 'きょおと'.kana2hepburn
-p 'とおる'.kana2hepburn
-p 'さいとう'.kana2hepburn
-p 'はっとり'.kana2hepburn
-p 'ほっち'.kana2hepburn
+puts '通常の変換'
+puts 'あいうえお かきくけこ'.kana2hepburn
+puts 'きゃきゅきょ'.kana2hepburn
+
+puts '撥音(ん)の通常処理と例外'
+puts 'あんこ'.kana2hepburn
+puts 'どらえもん'.kana2hepburn
+puts 'かんぽ'.kana2hepburn
+puts 'ほんま'.kana2hepburn
+
+puts '拗音(っ)の通常処理と例外処理'
+puts 'へっぽこ'.kana2hepburn
+puts 'べっぷ'.kana2hepburn
+puts 'いっしき'.kana2hepburn
+puts 'えっちゅう'.kana2hepburn
+puts 'はっちょう'.kana2hepburn
+
+puts '長音("お"や"う")の処理'
+puts 'うさぎ'.kana2hepburn
+puts 'たろう'.kana2hepburn
+puts 'おおさま'.kana2hepburn
+puts 'きょうと'.kana2hepburn
+puts 'きょおと'.kana2hepburn
+puts 'とおる'.kana2hepburn
+puts 'さいとう'.kana2hepburn
+
